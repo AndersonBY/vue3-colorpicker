36be026f493e17411e77bd9fa1804a9ac73bc30d
Anderson <andersonby@163.com>
2024-11-21 15:47:27 +0800

    fix: imports Vue3ColorPicker correctly

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import Vue3ColorPicker from "../packages";
+import { Vue3ColorPicker } from "../packages";
 
 createApp(App).use(Vue3ColorPicker).mount("#app");
