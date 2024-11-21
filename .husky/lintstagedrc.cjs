47fca6c3b8f349dba5b09e97f1186ef91a063097
Anderson <andersonby@163.com>
2024-11-21 15:18:00 +0800

    chore: updates lint-staged config

--- .husky/lintstagedrc.cjs
+++ .husky/lintstagedrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   "*.{js,jsx,ts,tsx}": ["eslint --cache --fix", "prettier --write"],
   "*.{css,scss,less,sass}": ["stylelint --cache --fix", "prettier --write"],
-  "{!(package)*.json,*.code-snippets,.!(browserslist)*rc}": ["prettier --write--parser json"],
+  "{!(package)*.json,*.code-snippets,.!(browserslist)*rc}": ["prettier --write --parser json"],
   "package.json": ["prettier --write"],
   "*.vue": ["eslint --fix", "prettier --write", "stylelint --fix"],
   "*.{scss,less,styl,html,css}": ["stylelint --fix", "prettier --write"],
